5be520388c3a09c551acd1b94386fc905ab4bfd7
Difrat <bozhenko_m_v@bk.ru>
2023-10-14 22:42:41 +1000

    Все таки захватывает некоторые паки. Нужно поработать над условиями или используемыми методами.

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ class Sorter:
     def get_directory(self) -> str:
         return self.directory
 
-    def set_exp(self) -> None:
-        file_list = os.listdir(self.directory)
+    def set_exp_list(self) -> None:
+        file_list = sorted(os.listdir(self.directory),
+                           key=lambda x: os.path.isdir(self.directory + f'/{x}'))
+        # file_list = sorted(os.listdir(self.directory), key=lambda x: os.path.isfile(self.directory + f'/{x}'))
         for item in file_list:
             exp = item.split('.')
-            if len(exp) == 2:
-                exp = exp[-1]
-                if exp not in self.exp_list:
-                    self.exp_list.append(exp)
+            if exp[-1] not in self.exp_list and len(exp) == 2:
+                self.exp_list.append(exp[-1])
 
     def create_directory(self):
         try:
-            for dir_name in s.exp_list:
-                os.mkdir(s.directory + f'/{dir_name}')
+            for dir_name in self.exp_list:
+                os.mkdir(self.directory + f'/{dir_name}')
         except FileExistsError:
             print('!!!Attention!!! -> The directory already exists')
 
@@ -34,19 +34,19 @@ class Sorter:
         file_list = os.listdir(self.directory)
         for item in file_list:
             try:
-                os.rename(f'{self.get_directory()}' + f'/{item}', f'{self.get_directory()}' + f'/{item.split(".")[-1]}'
-                                                                                              f'/{item}')
+                os.rename(f'{self.get_directory()}' + f'/{item}', f'{self.directory}' + f'/{item.split(".")[-1]}'
+                                                                                        f'/{item}')
             except Exception:
                 pass
 
 
 # Need to fix sort
-
+# Next iteration but
 if __name__ == '__main__':
     s = Sorter()
     s.set_directory()
     print(s.directory)
-    s.set_exp()
+    s.set_exp_list()
     print(s.exp_list)
     s.create_directory()
     s.sorting_files_to_folders()
