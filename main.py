3d4ea9b0ea2cb476313d032deffe0a5dcb8653a4
Difrat <bozhenko_m_v@bk.ru>
2023-10-19 22:57:38 +1000

    Добавил атрибут, метод и проработал простейшие исключения и проверки. Необходимо проработать еще исключения и логику программы.

--- main.py
+++ main.py
@@ -4,35 +4,35 @@ path = r'C:\Users\Difrat\Downloads'
 
 
 class Sorter:
-    def __init__(self, directory: str = None, exp_list: list = []):
+    def __init__(self, directory: str = None, exp_list: list = [], file_list: list = None):
         self.directory = directory
         self.exp_list = exp_list
+        self.file_list = file_list
 
     def set_directory(self) -> None:
         directory = input('Введите путь к интересующей директории: ')
         if os.path.exists(directory):
             self.directory = directory
-        else:
-            print(f'Путь не существует или указан неверно.')
+
+    def set_file_list(self):
+        self.file_list = [item for item in os.listdir(self.directory) if os.path.isfile(self.directory + f'/{item}')]
 
     def get_directory(self) -> str:
         return self.directory
 
     def set_exp_list(self) -> None:
-        file_list = [item for item in os.listdir(self.directory) if os.path.isfile(self.directory + f'/{item}')]
-        for item in file_list:
-            exp = item.split('.')
-            if exp[-1] not in self.exp_list:
-                self.exp_list.append(exp[-1])
+        if len(self.file_list) > 0:
+            for item in self.file_list:
+                exp = item.split('.')
+                if exp[-1] not in self.exp_list:
+                    self.exp_list.append(exp[-1])
 
-    def create_directory(self):
-        try:
-            for dir_name in self.exp_list:
+    def create_directory(self) -> None:
+        for dir_name in self.exp_list:
+            if not os.path.exists(self.directory + f'/{dir_name}'):
                 os.mkdir(self.directory + f'/{dir_name}')
-        except FileExistsError:
-            print('!!!Attention!!! -> The directory already exists')
 
-    def sorting_files_to_folders(self):
+    def sorting_files_to_folders(self) -> None:
         file_list = os.listdir(self.directory)
         for item in file_list:
             try:
@@ -45,6 +45,9 @@ class Sorter:
 if __name__ == '__main__':
     s = Sorter()
     s.set_directory()
-    # s.set_exp_list()
-    # s.create_directory()
-    # s.sorting_files_to_folders()
+    if s.directory:
+        s.set_exp_list()
+    else:
+        print(f'Директория не существует или указана неверно')
+    s.create_directory()
+    s.sorting_files_to_folders()
