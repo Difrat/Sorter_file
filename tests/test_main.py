16cd8a6c510a33f33658891eaa7845b11e7a5f38
Difrat <bozhenko_m_v@bk.ru>
2023-11-12 23:06:38 +1000

    Добавил немного тестирования.

---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import patch
+from main import Sorter
+
+
+class MainTest(unittest.TestCase):
+
+    @patch('builtins.input', return_value=r'C:\Users\Difrat\Downloads')
+    def test_Sorter_set_directory(self, mock_input):
+        sorter = Sorter()
+        sorter.set_directory()
+        self.assertEqual('C:\\Users\\Difrat\\Downloads', sorter.directory)
+
+    @patch('builtins.input', return_value=r'C:\Users\Difrat\Downl')
+    def test_exception_Sorter_set_directory(self, mock_input):
+        sorter = Sorter()
+        # sorter.set_directory()
+        with self.assertRaises(SystemExit):
+            sorter.set_directory()
