13a2e8f683cbf5a9800912e5b7b4115b17df9080
Difrat <bozhenko_m_v@bk.ru>
2023-11-13 22:20:06 +1000

    8 попытка автоматизации

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,13 @@ from main import Sorter
 
 class MainTest(unittest.TestCase):
 
-    @patch('builtins.input', return_value=r'C:\Users\Difrat\Downloads')
+    @patch('builtins.input', return_value='/home/runner/work/')
     def test_Sorter_set_directory(self, mock_input):
         sorter = Sorter()
         sorter.set_directory()
         self.assertEqual('C:\\Users\\Difrat\\Downloads', sorter.directory)
 
-    @patch('builtins.input', return_value=r'C:\Users\Difrat\Downl')
+    @patch('builtins.input', return_value='/home/runner/wor')
     def test_exception_Sorter_set_directory(self, mock_input):
         sorter = Sorter()
         with self.assertRaises(SystemExit):
