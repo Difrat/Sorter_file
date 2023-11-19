534247847c0f4161243939f1f8d5d6b299e6effd
Difrat <bozhenko_m_v@bk.ru>
2023-11-19 22:22:50 +1000

    Использовал метод subtest для параметризации теста

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,32 @@
 import unittest
+import os
 from unittest.mock import patch
 from main import Sorter
 
 
 class MainTest(unittest.TestCase):
 
-    @patch('builtins.input', return_value='/home/runner/work/')
+    @patch('builtins.input', return_value=r'C:\Users\Difrat\Downloads')
     def test_Sorter_set_directory(self, mock_input):
         sorter = Sorter()
         sorter.set_directory()
-        self.assertEqual('/home/runner/work/', sorter.directory)
+        self.assertEqual(r'C:\Users\Difrat\Downloads', sorter.directory)
 
-    @patch('builtins.input', return_value='/home/runner/wor')
+    @patch('builtins.input', return_value=r'C:\Users\Difrat\Download')
     def test_exception_Sorter_set_directory(self, mock_input):
         sorter = Sorter()
         with self.assertRaises(SystemExit):
             sorter.set_directory()
+
+    @patch('builtins.input', return_value=r'C:\Users\Difrat\Downloads')
+    def test_set_file_list(self, mock_input):
+        sorter = Sorter()
+        sorter.set_directory()
+        sorter.set_file_list()
+        print(sorter.file_list)
+        print(tuple(os.listdir(sorter.directory)))
+        test_cases = os.listdir(sorter.directory)
+
+        for file in test_cases:
+            with self.subTest(input_string=file):
+                self.assertIn(file, os.listdir(sorter.directory))
